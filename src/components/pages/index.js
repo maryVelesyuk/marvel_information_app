42eed6ad84f25bd739ca5cbd531036d1dd919b2d
maryVelesyuk <maryvelesyuk@gmail.com>
2022-07-08 13:41:19 +0300

    Updates

--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MainPage from "./MainPage";
-import ComicsPage from "./ComicsPage";
-import Page404 from "./404";
-import SingleComicPage from "./SingleComicPage";
-
-export { MainPage, ComicsPage, Page404, SingleComicPage };
